6c10ff00ac346aea8cf5f00586b7e8d95ef33b9c
Sipang K <plaaom39@gmail.com>
2023-06-18 12:30:51 +0700

    chore: update ErrorPage.tsx

--- frontend/src/pages/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage.tsx
@@ -1,13 +1,14 @@
-import { useRouteError } from "react-router-dom";
+// import { useRouteError } from "react-router-dom";
 
 function ErrorPage() {
-  // const error: unknown = useRouteError();
+  // const error = useRouteError();
   // console.log(error);
 
   return (
     <div className="flex flex-col gap-8 justify-center items-center h-screen">
       <h1 className="text-4xl font-bold">Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
+      <p className="text-lg font-bold">404 Not Found.</p>
       {/* <p className="text-slate-400">
         <i>{(error as Error)?.message || (error as { statusText?: string })?.statusText}</i>
       </p> */}
